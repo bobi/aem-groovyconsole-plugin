ec59ddd85625bed1fc554dbe8bb80120d8e077d3
GitHub Action <action@github.com>
2022-08-03 21:43:50 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bobi/aemgroovyconsoleplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bobi.aemgroovyconsoleplugin.services
+
+import com.github.bobi.aemgroovyconsoleplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
