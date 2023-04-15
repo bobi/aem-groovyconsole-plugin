38374a91212d81f7bae12e15979100eb4274bc0c
bobi <andrey.bardashevsky@gmail.com>
2023-04-15 20:02:20 +0200

    ### Added - Added plugin icon
    ### Changed
    - Update minimum supported Intellij version
    - Update usage of some Intellij deprecated APIs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
+
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun properties(key: String) = providers.gradleProperty(key)
 fun environment(key: String) = providers.environmentVariable(key)
@@ -31,7 +31,7 @@ dependencies {
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
 kotlin {
-    jvmToolchain(properties("javaVersion").get().toInt())
+    jvmToolchain(17)
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
@@ -65,16 +65,6 @@ kover.xmlReport {
 tasks {
     wrapper {
         gradleVersion = properties("gradleVersion").get()
-    }
-
-    properties("javaVersion").get().let {
-        withType<JavaCompile> {
-            sourceCompatibility = it
-            targetCompatibility = it
-        }
-        withType<KotlinCompile> {
-            kotlinOptions.jvmTarget = it
-        }
     }
 
     prepareSandbox {
